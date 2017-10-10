4a655c84d24a19b1a9373a86dc9183a0ab77e39f
Jesse Riggins <jdriggin@umail.iu.edu>
2017-10-10 13:59:13 -0400

    Added structure to dynamically create quiz question objects from a text file. Moved Taver Tycoon, Just One Jump, and Raceway to the "Minigames" folder.

---
+++ stuDYING/stuDYING/Assets/CreateArrayFromText.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateArrayFromText : MonoBehaviour {
+
+    public TextAsset dataFile;
+
+    public string[][] dataPairs;
+    public List<GameObject> listOfQuestionObjects;
+
+    public Object questionPrefab;
+
+    private float questionYValue = 0;
+    public float distanceBetweenQuestions = 200;
+
+	// Use this for initialization
+	void Start () {
+        /*
+           Format is: 
+           Question 1? / Correct Answer/ False Answer 1 / False Answer 2 / False Answer 3
+           Question 2? / Correct Answer/ False Answer 1 / False Answer 2 / False Answer 3
+           Question 3? / Correct Answer/ False Answer 1 / False Answer 2 / False Answer 3
+         */
+
+
+        string[] dataLines = dataFile.text.Split('\n');
+        dataPairs = new string[dataLines.Length][];
+        int lineNum = 0;
+        foreach (string line in dataLines)
+        {
+            dataPairs[lineNum++] = line.Split('/');
+        }
+        //Debug.Log(dataPairs[0][0]);
+        //Debug.Log(dataPairs[1][1]);
+
+        questionYValue = this.transform.position.y;
+
+        foreach (string[] d in dataPairs)
+        {
+            GameObject q = Instantiate(questionPrefab, new Vector3(0, questionYValue, 0), new Quaternion (0,0,0,0), this.gameObject.transform) as GameObject;
+            q.GetComponent<ExamMultipleChoiceQuestion>().questionList = d;
+            listOfQuestionObjects.Add(q);
+            questionYValue -= distanceBetweenQuestions;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ stuDYING/stuDYING/Assets/ExamMultipleChoiceQuestion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExamMultipleChoiceQuestion : MonoBehaviour {
+
+    public string[] questionList;
+
+    public Text questionDisplay;
+    public Text correctAnswerDisplay;
+    public Text falseAnswerDisplay;
+    public Text falseAnswerDisplay2;
+    public Text falseAnswerDisplay3;
+
+    // Use this for initialization
+    void Start () {
+        /*for (int i = 0; i < alpha.Count; i++) {
+         string temp = alpha[i];
+         int randomIndex = Random.Range(i, alpha.Count);
+         alpha[i] = alpha[randomIndex];
+         alpha[randomIndex] = temp;
+        }*/
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (questionList.Length > 0)
+        {
+            questionDisplay.text = questionList[0];
+            correctAnswerDisplay.text = questionList[1];
+            falseAnswerDisplay.text = questionList[2];
+            falseAnswerDisplay2.text = questionList[3];
+            falseAnswerDisplay3.text = questionList[4];
+        }
+    }
+}
