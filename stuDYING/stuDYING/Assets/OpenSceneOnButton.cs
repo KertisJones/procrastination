e9cd12a0ce510192d5f4cb5c8c5fe071b33e055c
Jesse Riggins <jdriggin@umail.iu.edu>
2017-10-19 17:10:30 -0400

    Notepad now lets you actually type things in now. Added super secret buttons to switch between Windows (i), Classroom (o), and Exam (p).

---
+++ stuDYING/stuDYING/Assets/OpenSceneOnButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenSceneOnButton : MonoBehaviour {
+
+    public string[] sceneNames;
+    public string[] keysToOpenScene;
+
+    //public float delayTime = 3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int i = 0;
+		while (i < keysToOpenScene.Length)
+        {
+            if (Input.GetKeyDown(keysToOpenScene[i]))
+            {
+                //yield return new WaitForSeconds(delayTime - .75f);
+                //float fadeTime = GameObject.Find("_GM").GetComponent<Fading>().BeginFade(1);
+                //yield return new WaitForSeconds(.75f);
+                SceneManager.LoadScene(sceneNames[i]);
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            i += 1;
+        }
+	}
+}
---
+++ stuDYING/stuDYING/Assets/disableOnExam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class disableOnExam : MonoBehaviour {
+
+    public string sceneThatDisables = "Exam";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sceneThatDisables == SceneManager.GetActiveScene().name)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
