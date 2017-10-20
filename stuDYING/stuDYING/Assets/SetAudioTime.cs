98613b1f3de0641bc8f72c2246cdd8f7757e8365
Jesse Riggins <jdriggin@umail.iu.edu>
2017-10-20 03:19:18 -0400

    I somehow made the skeleton gag even better.

---
+++ stuDYING/stuDYING/Assets/SetAudioTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetAudioTime : MonoBehaviour {
+
+    public float time = 0;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<AudioSource>().time = time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
